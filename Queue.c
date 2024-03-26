efbe4109a71eb73090172f31af75ea5bc8f83bcc
Fox_the_Engineer <164223569+Last-Caesar@users.noreply.github.com>
2024-03-26 13:36:23 +0300

    Update Queue.c

--- Queue.c
+++ Queue.c
@@ -1,30 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <conio.h>
 #define SIZE 6
+
+int pushQueue(int *arr, int v, int *First, int *Last, int *F, int *E, int n);
+int popQueue(int *arr, int *v, int *First, int *Last, int *F, int *E, int n);
 
 int main()
 {
-	int arr[SIZE] = { 0 };
+	int arr[SIZE] = { 0 }, value;
 
 	int First = 0, Last = 0;
 	int Full = 0, Empty = 1;
 
-	int value = 1;
-	pushQueue(arr, value, &First, &Last, &Full, &Empty, SIZE);
-	value = 2;
-	pushQueue(arr, value, &First, &Last, &Full, &Empty, SIZE);
-	int i;
-	for (i = 0; i < 3; i++){
-		pushQueue(arr, value, &First, &Last, &Full, &Empty, SIZE);
-	}
-
-	for (i = 0; i < SIZE; i++){
-		printf("%d\t", arr[i]);
-	}
-	printf("\n");
-
-	for (i = 0; i < 4; i++){
-		popQueue(arr, &value, &First, &Last, &Full, &Empty, SIZE);
-		printf("%d\n", value);
+	printf("Press a key: Esc - to exit | 1 - to push a value in queue | 2 - to pop a value from queue\n");
+	int exit = 0;
+	while (!exit) {
+		int c = _getch();
+		switch (c)
+		{
+		case '1':
+			printf("Enter a number\n");
+			scanf("%d", &value);
+			if (pushQueue(arr, value, &First, &Last, &Full, &Empty, SIZE) == -1)
+				printf("Error: Queue is full\n");;
+			break;
+		case '2':
+			if (popQueue(arr, &value, &First, &Last, &Full, &Empty, SIZE) == -1)
+				printf("Error: Queue is empty\n");
+			else
+				printf("%d\n", value);
+			break;
+		case 27: // 27 mean "Esc"
+			exit = 1;
+			break;
+		default:
+			break;
+		}
 	}
 
 
@@ -33,23 +45,31 @@ int main()
 int pushQueue(int *arr, int v, int *First, int *Last, int *F, int *E, int n)
 {
 	if (*F == 1){
-		printf("Queue is full");
 		return -1;
 	}
 	arr[*Last] = v;
 	++*Last;
 	*E = 0;
+	*Last %= n;
+	if (*Last == *First){
+		*F = 1;
+	}
 	return 0;
 }
 
 int popQueue(int *arr, int *v, int *First, int *Last, int *F, int *E, int n)
 {
 	if (*E == 1){
-		printf("Queue is full");
 		return -1;
 	}
 	*v = arr[*First];
 	++*First;
 	*F = 0;
+	*First %= n;
+	if (*Last == *First){
+		*E = 1;
+	}
 	return 0;
 }
+
+
